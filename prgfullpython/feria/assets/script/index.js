1db513d885de49f2197167d7d32b93d0830d4354
BJavierG <bjaviergg@gmail.com>
2023-05-26 18:17:47 -0300

    se ajusto la pagina para responsive, se agrego la animacion

--- prgfullpython/feria/assets/script/index.js
+++ prgfullpython/feria/assets/script/index.js
@@ -1,10 +1,18 @@
-var barra = `<nav>
-            <a href="../.././index.html">Inicio </a>
-            <a href="/assets/pantallas/productos.html">Productos</a>
-            <a href="/assets/pantallas/carrito_de_compras.html">Tienda</a>
-            <a href="/assets/pantallas/registrarse.html">Registrarse</a>
-            <a href="/assets/pantallas/contacto.html">Contacto</a>
-            <a href="/assets/pantallas/sobrenosotros.html">Sobre nosotros</a>
-        </nav>`
+var barra = `
+<div>
+    <input type="checkbox">
+    <i class="fas fa-bars"></i>
+    <i class="fas fa-times"></i>
+        <nav>
+            <ul>
+                <li><a href="/feria/index.html">Inicio </a></li>
+                <li><a href="/feria/assets/pantallas/productos.html">Productos</a></li>
+                <li><a href="/feria/assets/pantallas/carrito_de_compras.html">Tienda</a></li>
+                <li><a href="/feria/assets/pantallas/registrarse.html">Registrarse</a></li>
+                <li><a href="/feria/assets/pantallas/contacto.html">Contacto</a></li>
+                <li><a href="/feria/assets/pantallas/sobrenosotros.html">Sobre nosotros</a></li>
+            </ul>
+        </nav>
+</div>`
 
 document.getElementById("navBarId").innerHTML = barra;
