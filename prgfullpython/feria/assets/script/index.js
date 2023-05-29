460c3a342a2863340775e745f0581b6a01a7d60c
Jorge <castagnajoc@gmail.com>
2023-05-29 18:04:31 -0300

    modificaciones realizadas por Javier y Jorge

--- prgfullpython/feria/assets/script/index.js
+++ prgfullpython/feria/assets/script/index.js
@@ -7,11 +7,12 @@ var barra = `
             <ul>
                 <li><a href="/feria/index.html">Inicio </a></li>
                 <li><a href="/feria/assets/pantallas/productos.html">Productos</a></li>
-                <li><a href="/feria/assets/pantallas/carrito_de_compras.html">Tienda</a></li>
+                <li><a href="/feria/assets/pantallas/tienda.html">Tienda</a></li>
                 <li><a href="/feria/assets/pantallas/registrarse.html">Registrarse</a></li>
                 <li><a href="/feria/assets/pantallas/contacto.html">Contacto</a></li>
                 <li><a href="/feria/assets/pantallas/sobrenosotros.html">Sobre nosotros</a></li>
-            </ul>
+                <li><a href="/feria/assets/pantallas/carrito_de_compras.html">Carrito de compras</a></li>
+                </ul>
         </nav>
 </div>`
 
