9da4b965cfba36555f46c88dfd7f38eed07237be
BJavierG <bjaviergg@gmail.com>
2023-06-16 11:29:54 -0300

    se corrigio consumo de API

--- prgfullpython/feria/assets/script/carrito.js
+++ prgfullpython/feria/assets/script/carrito.js
@@ -1,55 +1,44 @@
-// const URI_ENDPOINT = "https://sheetdb.io/api/v1/v8sid17znpqza";
 
-// fetch(URI_ENDPOINT)
-//     .then((datos) => datos.json())
-//     .then((data) => console.log(data));
+fetch('https://sheetdb.io/api/v1/v8sid17znpqza')
+  .then(response => response.json()) // Parsear la respuesta como JSON
+  .then(data => {
+    // Aquí puedes trabajar con los datos obtenidos de la API
+    console.log(data); // Verifica que los datos se estén obteniendo correctamente
 
-import productos from "/feria/assets/db/productos.json" assert {type : "json"};
-
-// console.log(productos);
-
-class Product {
-    constructor(articulo, precio, img) {
-        (this.articulo = articulo), (this.precio = precio), (this.img = img);
-    }
-    getDivCardProduct(idToInsert) {
-        /*selecciona el elementro padre*/
-        let parent = document.getElementById(idToInsert);
-        /*crea un elemento a indexar o inyectar*/
-        let div = document.createElement("div");
-        /*le agreo lo que inyecto*/
-        div.innerHTML = `
-        <center>
-            <h4>${this.articulo}</h4>
-            <div>
-                <img src="${this.img}" alt="${this.name}" />
-                <h5> ${this.precio} </h5>
-            </div>
-        </center>`;    
-        /* agrego elemento hijo  al padre*/
-        parent.appendChild(div);
-    }
-}
-document.addEventListener("DOMContentLoaded", function() {
-    
-    var productObjeto = productos.map(
-        (producto) => new Product(producto.articulo, producto.precio, producto.img),
+    // Crear instancias de la clase Product y mostrar los datos en el HTML
+    var productObjeto = data.map(
+      (producto) => new Product(producto.articulo, producto.precio, producto.img),
     );
 
-    productos.forEach((producto) => console.log(`${producto.articulo}`));
-
     productObjeto.forEach((art) => art.getDivCardProduct("grid-container"));
-    });
+  })
+  .catch(error => {
+    // Manejo de errores en caso de que la petición falle
+    console.error('Error:', error);
+  });
 
+class Product {
+  constructor(articulo, precio, img) {
+    this.articulo = articulo;
+    this.precio = precio;
+    this.img = img;
+  }
 
+  getDivCardProduct(idToInsert) {
+    let parent = document.getElementById(idToInsert);
+    let div = document.createElement("div");
+    div.innerHTML = `
+      <center>
+        <h4>${this.articulo}</h4>
+        <div>
+          <img src="/prgfullpython/${this.img}" alt="${this.name}" />
+          <h5>${this.precio}</h5>
+        </div>
+      </center>`;
+    parent.appendChild(div);
+  }
+}
 
-// var tienda = "`";
-
-// for (let art of productos){
-    
-//     tienda += '<div class="grid-item"><img src= "${art.img}" alt="${art.articulo}"><br>"${art.articulo}"<br>"${art.precio}"</div>'
-// }
-// tienda += "`"
-
-// console.log(tienda)
-// document.getElementById("grid-container").innerHTML = tienda;
+document.addEventListener("DOMContentLoaded", function() {
+  // El código dentro de esta función se ejecutará cuando se cargue el DOM
+});
--- prgfullpython/feria/assets/script/index.js
+++ prgfullpython/feria/assets/script/index.js
@@ -5,13 +5,13 @@ var barra = `
     <i class="fas fa-times"></i>
         <nav>
             <ul>
-                <li><a href="/feria/index.html">Inicio </a></li>
-                <li><a href="/feria/assets/pantallas/productos.html">Productos</a></li>
-                <li><a href="/feria/assets/pantallas/tienda.html">Tienda</a></li>
-                <li><a href="/feria/assets/pantallas/registrarse.html">Registrarse</a></li>
-                <li><a href="/feria/assets/pantallas/contacto.html">Contacto</a></li>
-                <li><a href="/feria/assets/pantallas/sobrenosotros.html">Sobre nosotros</a></li>
-                <!--<li><a href="/feria/assets/pantallas/carrito_de_compras.html">Carrito de compras</a></li>-->
+                <li><a href="/prgfullpython/feria/index.html">Inicio </a></li>
+                <li><a href="/prgfullpython/feria/assets/pantallas/productos.html">Productos</a></li>
+                <li><a href="/prgfullpython/feria/assets/pantallas/tienda.html">Tienda</a></li>
+                <li><a href="/prgfullpython/feria/assets/pantallas/registrarse.html">Registrarse</a></li>
+                <li><a href="/prgfullpython/feria/assets/pantallas/contacto.html">Contacto</a></li>
+                <li><a href="/prgfullpython/feria/assets/pantallas/sobrenosotros.html">Sobre nosotros</a></li>
+                <!--<li><a href="/prgfullpython/feria/assets/pantallas/carrito_de_compras.html">Carrito de compras</a></li>-->
                 </ul>
         </nav>
 </div>`
